2d33d7089aad34b5ee22e80f3d03724f31a99e1a
Daryl Bacurin <darylbacurin14@gmail.com>
2024-06-04 00:11:14 +0800

    Test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/frontend_mentor/",
+    element: <HomePage />,
+  },
+  {
+    path: "/frontend_mentor/profile_page",
+    element: <ProfilePage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
