7def4b75d7e7e5dd3cb9ea51df681dc020329e31
Daryl Bacurin <darylbacurin14@gmail.com>
2024-06-06 00:11:29 +0800

    Test imports

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <Link to="/frontend_mentor/projects">To Projects</Link>
     </div>
   );
 }
